23fd9b8a351b458f127a25d1e941f55d09dc8186
Marselino-Geradus <marselino914@gmail.com>
2022-10-19 19:25:51 +0800

    Ini hari ketiga

---
+++ Operator Aritmatika, Penugasan, & Perbandingan.py
@@ -0,0 +1,52 @@
+#Operator Aritmetika
+print("Operator Aritmetika")
+a = 12
+b = 4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**2)
+print("-----------------------")
+
+#Operator Penugasan
+print("Operator Penugasan")
+c = 10
+c += 2
+print(c)
+
+d = 10
+d -= 5
+print(d)
+
+e = 10
+e *= 3
+print(e)
+
+f = 10
+f /= 2
+print(f)
+
+g = 10
+g **= 3
+print(g)
+
+h = 10
+h %= 4
+print(h)
+
+print("-----------------------")
+
+#Operator Perbandingan
+print("Operator Perbandingan")
+data1 = 12
+data2 = 10
+data3 = "makan"
+
+print(data1 == data2)
+print(data1 != data2)
+print(data1 >= data2)
+print(data1 <= data2)
+print(data1 > data2)
+print(data1 < data2)
